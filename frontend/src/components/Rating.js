d0f3fb0a7f93363da309f613ca73f9344b0870be
Florian Soller <flsoller@gmail.com>
2020-10-03 14:30:46 +0200

    Add rating.js

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Rating = ({ rating, numReviews }) => {
+  return (
+    <div className="rating">
+      {rating >= 1 ? (
+        <i className="fas fa-star"></i>
+      ) : rating >= 0.5 ? (
+        <i className="fas fa-star-half-alt"></i>
+      ) : (
+        <i className="far fa-star"></i>
+      )}
+      {rating >= 2 ? (
+        <i className="fas fa-star"></i>
+      ) : rating >= 1.5 ? (
+        <i className="fas fa-star-half-alt"></i>
+      ) : (
+        <i className="far fa-star"></i>
+      )}
+      {rating >= 3 ? (
+        <i className="fas fa-star"></i>
+      ) : rating >= 2.5 ? (
+        <i className="fas fa-star-half-alt"></i>
+      ) : (
+        <i className="far fa-star"></i>
+      )}
+      {rating >= 4 ? (
+        <i className="fas fa-star"></i>
+      ) : rating >= 3.5 ? (
+        <i className="fas fa-star-half-alt"></i>
+      ) : (
+        <i className="far fa-star"></i>
+      )}
+      {rating >= 5 ? (
+        <i className="fas fa-star"></i>
+      ) : rating >= 4.5 ? (
+        <i className="fas fa-star-half-alt"></i>
+      ) : (
+        <i className="far fa-star"></i>
+      )}
+      <p>{numReviews} Reviews</p>
+    </div>
+  );
+};
+
+export default Rating;
